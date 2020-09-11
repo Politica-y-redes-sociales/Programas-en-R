7b888f0348e990b166197dd4eafef40237feb6db
Claudio Diaz <cristobal.4.diaz@gmail.com>
2020-09-11 18:00:57 -0300

    actulizacion prensaTweets

--- Prensa/prensaTweets.R
+++ Prensa/prensaTweets.R
@@ -9,8 +9,10 @@ nombresBases = dir(carpeta_base);
 nombresBases = as.data.frame(nombresBases)
 
 prensa=paste(carpeta,'queriesprensa.csv',sep = '/')
-prensa=read.csv(prensa,sep = ';', encoding = "UTF-8")
+prensa=read.csv(prensa,sep = ';')
 cuentasPrensa=prensa[c('Nombre','Carpeta')]
+
+
 
 carpetaResultado=paste(carpeta,"resultados",sep="/")
 
@@ -18,18 +20,22 @@ if(!dir.exists(carpetaResultado)){
   dir.create(carpetaResultado);
 }
 
-for (i in nombresBases[,1]) 
-{
+for (i in nombresBases[,1]) {
+  
   
   file=paste(carpeta_base,i,sep = '/');
   file=read.csv(file);
   file <- as.data.frame(file);
   cuentasPrensa <- as.data.frame(cuentasPrensa)
   view(cuentasPrensa)
-    query <- paste('SELECT screen_name,text FROM cuentasPrensa 
-                   INNER JOIN file ON cuentasPrensa.nombre = file.screen_name WHERE cuentasPrensa.Carpeta = "Prensa" ')
+  
+    query <- paste('select screen_name as usuario,text as tweet FROM cuentasPrensa INNER JOIN file ON cuentasPrensa.nombre = file.screen_name where cuentasPrensa.Carpeta = "Prensa" ')
     tweets <- sqldf(query)
     tweets <- as.data.frame(tweets);
+    query1 <- paste('SELECT screen_name as usuario, count(text) as cantidad FROM cuentasPrensa join file on cuentasPrensa.nombre = file.screen_name where cuentasPrensa.Carpeta = "Prensa" group by screen_name')
+    contador <- sqldf(query1)
+    print(contador)
     view(tweets)
-    write.csv(tweets,file = paste(carpetaResultado,i,sep='/'), fileEncoding = "UTF-8")
+    write.csv(contador,file = paste(carpetaResultado,i,sep='/cantidad '))
+    write.csv(tweets,file = paste(carpetaResultado,i,sep='/'))
 }
