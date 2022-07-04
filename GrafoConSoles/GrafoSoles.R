92b4f8842ccbbffac0a80b15d8195d3bc375e345
Alfonso! <30917026+alfonsoopazom@users.noreply.github.com>
2022-07-03 21:25:40 -0400

    Update GrafoSoles.R

--- GrafoConSoles/GrafoSoles.R
+++ GrafoConSoles/GrafoSoles.R
@@ -10,7 +10,6 @@ library(tm)
 library(colorspace)
 library(sqldf)
 
-
 carpeta<-"C:/Users/Administrador/Desktop/ProgramasR/GrafosSoles"
 carpeta_Bases<-paste(carpeta,"Bases",sep="/")
 nombres<-dir(carpeta_Bases)
@@ -22,9 +21,7 @@ puntos=data.frame("Id"=character(),"Label"=character(),"Category"=character())
 if(dir.exists(paste(carpeta,"Resultados",sep = "/")))
 {}else{dir.create(paste(carpeta,"Resultados",sep = "/"))}
 
-for(i in 1:length(nombres[,1]))
-{
-  
+for(i in 1:length(nombres[,1])) {
   mencion=toString(nombres$nombres[i])
   archivo_temporal<-paste(carpeta_Bases,mencion,sep="/")
   nombre<-substr(toString(nombres$nombres[i]),1,(str_length(nombres$nombres[i])-4))
@@ -34,14 +31,10 @@ for(i in 1:length(nombres[,1]))
   
   consulta=sqldf("select screen_name as Source ,text as Target, retweet_screen_name as Rt from consulta")
   recuento=rbind(recuento,consulta)
-  
 }
-
-
 #Aristas(Source,Target,Weight)
 aristas=sqldf("select Source ,Target,count(*) as Weight from recuento group by Source,Target")
 #Nodos (Id,Label,Category)
-
 
 write.csv(aristas, row.names = FALSE, file = paste(carpeta,"Resultados/aristas.csv",sep = "/"))
 
@@ -50,16 +43,13 @@ recuento=sqldf("select Source as Id , Source as Label from recuento group by Sou
 nodos=rbind(nodos,recuento)
 nodos=sqldf("select Id,Label from nodos group by Id,Label ")
 
-for (i in nodos$Id)
-{
+for (i in nodos$Id) {
   frase=sqldf(paste("select Target from aristas where Source='","'",sep=i))
   palabra=""
-  if(length(frase[,1])==0)
-  {
+  if(length(frase[,1])==0) {
     palabra=paste(" ",i)
   }
-  for(j in frase$Target)
-  {
+  for(j in frase$Target) {
     palabra=paste(palabra,j,sep=" ")
   }
   tabla=as.data.frame(t(as.data.frame(c(as.character(i),as.character(i),as.character(palabra)))))
@@ -68,7 +58,6 @@ for (i in nodos$Id)
   names(tabla)[3]<-paste("Category")
   rownames(tabla) <- c()
   puntos=rbind(puntos,tabla)
-  
 }
 
 write.csv(puntos, row.names = FALSE, file = paste(carpeta,"Resultados/nodos.csv",sep = "/"))
